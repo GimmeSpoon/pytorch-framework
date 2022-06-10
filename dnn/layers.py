528527eafd7b1365c7eb5a04e18cdbcdf02e2351
unknown <ncw1541@o365.sogang.ac.kr>
2022-06-11 02:07:16 +0900

    Init

---
+++ dnn/layers.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+import numpy as np
+import pandas as pd
+from abc import ABC
+
+class Layer(nn.Module):
+    def __init__(self, **kargs) -> None:
+        pass
+    def train(self):
+        
+        pass
+    def forward(self):
+        pass
+
+class LSTM:
+    def __init__(self) -> None:
+        pass
+    def train(self):
+        pass
+    def train(self):
+        pass
+
---
+++ framework.py
@@ -0,0 +1,43 @@
+import argparse
+import string
+import pandas as pd
+import numpy as np
+import torch
+from torch import nn
+
+from torch import float32
+
+parser = argparse.ArgumentParser(prog="General Machine Learning Framework",
+                                 description="General ML framework for various use. It provides CLI interface with argparser and basic training and inference actions.")
+
+""" Basic config """
+parser.add_argument('mode')
+parser.add_argument('--input_path', type=string, metavar='INPUT_PATH')
+parser.add_argument('--output_path', type=string, metavar='OUTPUT_PATH')
+parser.add_argument('--checkpoint_path', type=string, metavar='CHECKPOINT_PATH')
+
+""" Hyperparameters """
+parser.add_argument('-l', '--lr', type=float32, default=0.001, metavar='LEARNING_RATE', help='initial learning rate', dest='learning_rate')
+
+""" You can also provide config values by file. If file path is passed, hyperparameters before will be ignored."""
+parser.add_argument('--config_path', type=string, default='', metavar='CONFIG_PATH')
+parser.add_argument('--config_type', choices=['xml', 'json', 'yaml'], default='yaml', metavar='CONFIG_TYPE')
+
+""" config file handle here """
+
+"""------------------------ """
+
+args = parser.parse_args()
+
+
+""" Data PreProcessing """
+
+""" ------------------ """
+
+"""              Train              """
+
+""" ------------------------------- """
+
+"""              Infer              """
+
+""" ------------------------------- """
